f7a968b81d690d50e2107d94099a8b48fdc492f6
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-08-19 07:46:59 +0300

    no message

--- Charge.php
+++ Charge.php
@@ -2,12 +2,17 @@
 namespace Dfe\PostFinance;
 /**
  * 2017-08-18
+ * 2017-08-19
+ * The charge parameters are described in the Chapter 4.2 «Form parameters» of the web documentation:
+ * https://e-payment-postfinance.v-psp.com/en/en/guides/integration%20guides/e-commerce#formparameters
  * @method Method m()
  * @method Settings s()
  */
 final class Charge extends \Df\PaypalClone\Charge {
 	/**
 	 * 2017-08-19
+	 * «Your affiliation name in our system»
+	 * `[PostFinance] What is my PSPID?` https://mage2.pro/t/4349
 	 * @override
 	 * @see \Df\PaypalClone\Charge::k_MerchantId()
 	 * @used-by \Df\PaypalClone\Charge::p()
@@ -17,12 +22,16 @@ final class Charge extends \Df\PaypalClone\Charge {
 
 	/**
 	 * 2017-08-18
+	 * 2017-08-19
+	 * «Your order number (merchant reference).
+	 * The system checks that a payment has not been requested twice for the same order.
+	 * The ORDERID has to be assigned dynamically.»
 	 * @override
 	 * @see \Df\PaypalClone\Charge::k_RequestId()
 	 * @used-by \Df\PaypalClone\Charge::p()
 	 * @return string
 	 */
-	protected function k_RequestId() {return '';}
+	protected function k_RequestId() {return 'ORDERID';}
 
 	/**
 	 * 2017-08-18
