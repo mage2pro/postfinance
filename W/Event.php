5030edc147de8bd78b88fcd40398d5f38ee41be2
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-08-29 21:55:21 +0300

    no message

--- W/Event.php
+++ W/Event.php
@@ -3,6 +3,8 @@ namespace Dfe\PostFinance\W;
 use Magento\Sales\Model\Order\Payment\Transaction as T;
 /**
  * 2017-08-29
+ * «Integrate with PostFinance e-Commerce» → «7. Transaction feedback»
+ * https://e-payment-postfinance.v-psp.com/en/en/guides/integration%20guides/e-commerce/transaction-feedback
  */
 final class Event extends \Df\PaypalClone\W\Event {
 	/**
--- W/Handler.php
+++ W/Handler.php
@@ -3,6 +3,8 @@ namespace Dfe\PostFinance\W;
 use Df\Framework\Controller\Result\Text;
 /**
  * 2017-08-29
+ * «Integrate with PostFinance e-Commerce» → «7. Transaction feedback»
+ * https://e-payment-postfinance.v-psp.com/en/en/guides/integration%20guides/e-commerce/transaction-feedback
  */
 final class Handler extends \Df\PaypalClone\W\Handler {
 	/**
