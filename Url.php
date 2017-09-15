b97367b64be8b4c98c3b215b5b00a8af8c7db007
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-16 02:04:45 +0300

    no message

--- Url.php
+++ Url.php
@@ -4,7 +4,8 @@ namespace Dfe\PostFinance;
 final class Url extends \Df\Payment\Url {
 	/**
 	 * 2017-08-19
-	 * Первый параметр — для test, второй — для live.
+	 * The method returns a 2-tuple:
+	 * the first element is for the test mode, the second is for the production mode.
 	 * https://e-payment-postfinance.v-psp.com/en/en/guides/integration%20guides/e-commerce#formaction
 	 * @override
 	 * @see \Df\Payment\Url::stageNames()
